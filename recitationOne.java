d7fd3851fe7d21e9c0667f2ed7288903a48d2d14
sigalsax <ssax18@gmail.com>
2017-09-12 18:31:07 -0400

    fix image alignment

---
+++ recitationOne.java
@@ -0,0 +1,19 @@
+public class recitationOne {
+	
+	public static void main (String [] args){
+		verseOne();
+		System.out.println();
+		verseTwo();
+	}
+
+	public static void verseOne(){
+		System.out.println("This is the house that Jack built");
+	}
+	public static void verseTwo(){
+		System.out.println("This is the malt");
+		verseThree();
+	}
+	public static void verseThree(){
+		System.out.println("That lay in the house that Jack built");
+	}
+}
